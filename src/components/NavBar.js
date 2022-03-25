20c90081456907f532b856ed1b3f3e7f10394ecd
yann-soaz <jeff.billot@gmail.com>
2022-03-25 11:20:44 +0100

    creattion de la navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const linkList = [
+    ['/', 'Home'],
+    ['/404', '404'],
+]
+
+const navBar = () => {
+    return (
+        <nav>
+            <img src="https://picsum.photos/150/100"/>
+            <div>
+                {
+                    linkList.map(
+                        ([url, titre], index) => <Link key={index} to={url}>{titre}</Link>
+                    )
+                }
+            </div>
+        </nav>
+    )
+}
+
+export default navBar;
