09dac28d465b4d0753b84a8b9974e8864c5aaad2
yann-soaz <jeff.billot@gmail.com>
2022-03-25 16:52:02 +0100

    pageNumber v2

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,27 +1,24 @@
 import { Link } from "react-router-dom";
 
+const prevNextPages = (pageLink, pagenumber, pagemax = 0, prev = true) => {
+    // les 3 pages avant la notre
+    for (let x = 1; x < 6; x++) { // je fais 3 tour de boucle
+        let pageLinkNumber = (prev) ? (pagenumber - x) : (pagenumber + x) ; // je calcul le nouveau numero de page du lien
+        if ((prev && pageLinkNumber < 1) || (!prev && pageLinkNumber > pagemax)) // si il est inférieur à 1
+            break; // j'arrète la boucle
+        pageLink.push(<Link to={"/"+pageLinkNumber}>{pageLinkNumber}</Link>) // sinon j'ajoute le lien a la liste
+    }
+    if (prev)
+        pageLink.reverse(); // je remets les pages dans le bon ordre
+    return pageLink;
+}
+
 const showPage = (pageMax, pagenumber) => {
     let pageLink = [];
 
-    // les 3 pages avant la notre
-    for (let x = 1; x < 4; x++) { // je fais 3 tour de boucle
-        let pageLinkNumber = pagenumber - x; // je calcul le nouveau numero de page du lien
-        if (pageLinkNumber < 1) // si il est inférieur à 1
-            break; // j'arrète la boucle
-        pageLink.push(<Link to={"/"+pageLinkNumber}>{pageLinkNumber}</Link>) // sinon j'ajoute le lien a la liste
-    }
-    pageLink.reverse(); // je remets les pages dans le bon ordre
-
+    pageLink = prevNextPages(pageLink, pagenumber) // les trois pages d'avant
     pageLink.push(<span className="actualPage">{pagenumber}</span>) // j'ajoute la page actuelle
-    
-    // les 3 pages après la notre
-    for (let x = 1; x < 4; x++) { // je fais 3 tour de boucle
-        let pageLinkNumber = pagenumber + x; // je calcul le nouveau numero de page du lien
-        if (pageLinkNumber > pageMax) // si il est inférieur à 1
-            break; // j'arrète la boucle
-        pageLink.push(<Link to={"/"+pageLinkNumber}>{pageLinkNumber}</Link>) // sinon j'ajoute le lien a la liste
-    }
-
+    pageLink = prevNextPages(pageLink, pagenumber, pageMax, false) // les trois pages d'après
 
     return pageLink;
 }
