886f1403ac499ac42d75d80e4644bcae27d88af3
yann-soaz <jeff.billot@gmail.com>
2022-03-25 13:15:20 +0100

    ajout liste personnage

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Accueil = () => {
-    let {pageNumber} = useParams();
-    console.log(pageNumber);
+    let {pageNumber} = useParams(); // j'utilise les paramètre de l'url -> /:pageNumber donne un objet avec l'attribut pageNumber
+    pageNumber = (typeof pageNumber == 'undefined') ? 1 : pageNumber // si pagenumber n'est pas dans l'url je considère être a la page 1
+    let [APIDatas, setAPIDatas] = useState([]) // par defaut ma page est vide dans sa liste de personnage
+    let [pageMax, setPageMax] = useState(1) // le maxpage est a 1 par défault mais peut evoluer
+    useEffect(
+        () => {
+            // aller faire un fetch sur la liste de parsonnage sur l'api Rick&Morty en fonction de pagenumber
+        }, [pageNumber]
+    )
     return (
-        <h1>Accueil</h1>
+        <div>
+            {
+                APIDatas.map(
+                    item => {
+                        <Perso datas={item} key={item.id} />
+                    }
+                )
+            }
+        </div>
     )
 }
 
