2af3d5a40c0238426a0c5d784a86021f6fa2d1f8
yann-soaz <jeff.billot@gmail.com>
2022-03-25 16:57:37 +0100

    key attribute

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const prevNextPages = (pageLink, pagenumber, pagemax = 0, prev = true) => {
     // les 3 pages avant la notre
-    for (let x = 1; x < 6; x++) { // je fais 3 tour de boucle
+    for (let x = 1; x < 4; x++) { // je fais 3 tour de boucle
         let pageLinkNumber = (prev) ? (pagenumber - x) : (pagenumber + x) ; // je calcul le nouveau numero de page du lien
         if ((prev && pageLinkNumber < 1) || (!prev && pageLinkNumber > pagemax)) // si il est inférieur à 1
             break; // j'arrète la boucle
-        pageLink.push(<Link to={"/"+pageLinkNumber}>{pageLinkNumber}</Link>) // sinon j'ajoute le lien a la liste
+        pageLink.push(<Link key={pageLink.length} to={"/"+pageLinkNumber}>{pageLinkNumber}</Link>) // sinon j'ajoute le lien a la liste
     }
     if (prev)
         pageLink.reverse(); // je remets les pages dans le bon ordre
@@ -17,7 +17,7 @@ const showPage = (pageMax, pagenumber) => {
     let pageLink = [];
 
     pageLink = prevNextPages(pageLink, pagenumber) // les trois pages d'avant
-    pageLink.push(<span className="actualPage">{pagenumber}</span>) // j'ajoute la page actuelle
+    pageLink.push(<span key={pageLink.length} className="actualPage">{pagenumber}</span>) // j'ajoute la page actuelle
     pageLink = prevNextPages(pageLink, pagenumber, pageMax, false) // les trois pages d'après
 
     return pageLink;
