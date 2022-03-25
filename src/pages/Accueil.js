ec2e2efe35b144ff269e07c93333abe32f7baf95
yann-soaz <jeff.billot@gmail.com>
2022-03-25 14:45:47 +0100

    ajustement fetch

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -6,17 +6,28 @@ const Accueil = () => {
     pageNumber = (typeof pageNumber == 'undefined') ? 1 : pageNumber // si pagenumber n'est pas dans l'url je considère être a la page 1
     let [APIDatas, setAPIDatas] = useState([]) // par defaut ma page est vide dans sa liste de personnage
     let [pageMax, setPageMax] = useState(1) // le maxpage est a 1 par défault mais peut evoluer
+    let [is404, set404] = useState(false)
     useEffect(
         () => {
             // aller faire un fetch sur la liste de parsonnage sur l'api Rick&Morty en fonction de pagenumber
             fetch('https://rickandmortyapi.com/api/character/?page='+pageNumber)
-                .then(response => response.json())
+                .then(response => {
+                    if (!response.ok) {
+                        set404(true); return;
+                    }
+                    return response.json()
+                })
                 .then(APIResult => {
                     setAPIDatas(APIResult.results)
                     setPageMax(APIResult.info.pages)
-                })
+                }).catch(err => set404(true));
         }, [pageNumber]
     )
+    
+    pageNumber = +(pageNumber); // je transforme le paramètre pageNumber en int
+    if (isNaN(pageNumber) || is404)
+        return (<h1>404</h1>)
+
     return (
         <div>
             {
