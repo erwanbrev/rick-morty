b92057cac338be47d4dd327431a7773315a744a4
yann-soaz <jeff.billot@gmail.com>
2022-03-29 09:25:37 +0200

    perso couleur favori

--- src/components/Perso.js
+++ src/components/Perso.js
@@ -1,19 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import FavoriteContext from '../FavoriteContext';
 import './Perso.css';
 
 const Perso = (props) => {
 
-    let {name, image} = props.persoDatas
-    let favs = useContext(FavoriteContext);
-    console.log(favs);
+    let {name, image, id} = props.persoDatas
+    let favContext = useContext(FavoriteContext);
+    let [isFav, setFav] = useState('');
+    
+    useEffect(
+        () => {
+            let find = false;
+            for (let persoFav of favContext.favs) {
+                if (persoFav.id != id) {
+                    continue; // passe au tour de boucle suivant sans executer la suite
+                }
+                find = true;
+                setFav(' active')
+                break; // met fin a la boucle
+            }
+            if (!find) {
+                setFav('')
+            }
+        }, [favContext]
+    )
     
     
     return (
         <article className="perso">
             <img src={image} />
             <h3>{name}</h3>
-            <span onClick={() => { favs.register(props.persoDatas) }} className="favorite"></span>
+            <span onClick={() => { favContext.register(props.persoDatas) }} className={"favorite"+isFav}></span>
         </article>
     )
 }
